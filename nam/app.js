e964098704104dd1dfeeeca63069e6f44c074b86
NaiduBagana <baganasatyavathi@gmail.com>
2025-02-26 13:58:40 +0530

    1st nama added

---
+++ nam/app.js
@@ -0,0 +1,14 @@
+// const he = React.createElement('h1', {id: "heading"}, 'Hello, world! ab');
+// const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+// rootElement.render(he);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", { id: "heading" }, "Hello, world! ab")
+//   )
+// );
+// const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+// rootElement.render(parent);
