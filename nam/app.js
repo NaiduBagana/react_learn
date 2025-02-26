961f853d8b3851318adcbdcdbed69327002f7e86
NaiduBagana <baganasatyavathi@gmail.com>
2025-02-26 19:05:01 +0530

    kk

--- nam/app.js
+++ nam/app.js
@@ -1,14 +1,16 @@
 // const he = React.createElement('h1', {id: "heading"}, 'Hello, world! ab');
 // const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 // rootElement.render(he);
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//     React.createElement("h1", { id: "heading" }, "Hello, world! ab")
-//   )
-// );
-// const rootElement = ReactDOM.createRoot(document.getElementById('root'));
-// rootElement.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "heading" }, "Hello, wohiii! ab")
+  )
+);
+const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+rootElement.render(parent);
